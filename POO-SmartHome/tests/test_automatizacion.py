1bbd465e54ac4fe7a6b76c245f7d63692bf8fdea
Federico Pierrestegui <144521724+pierresteguifederico@users.noreply.github.com>
2025-09-16 00:56:47 -0300

    se crean test para automatizaciones

---
+++ POO-SmartHome/tests/test_automatizacion.py
@@ -0,0 +1,26 @@
+from src.automatizacion import Automatizacion
+from src.dispositivo import Dispositivo
+from src.accion import Accion
+
+def test_automatizacion_con_dispositivos():
+    auto = Automatizacion("Prueba de automatización")
+    auto.agregar_accion(Accion("encender", None))
+    auto.agregar_accion(Accion("set_nombre", "Lámpara nueva"))
+
+    d1 = Dispositivo("Luz", "iluminacion")
+    d2 = Dispositivo("Ventilador", "clima")
+
+    resultados = auto.ejecutar([d1, d2])
+
+    # Debe haber 2 acciones * 2 dispositivos = 4 resultados
+    assert len(resultados) == 4
+
+    resultado_esperado = [
+    "El dispositivo Luz ha sido encendido.",
+    "El dispositivo Ventilador ha sido encendido.",
+    "El dispositivo ahora se llama Lámpara nueva.",
+    "El dispositivo ahora se llama Lámpara nueva."
+]
+    assert resultados == resultado_esperado
+
+
