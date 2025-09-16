cfa08f80543f9444bf42bc4e4af7caacb38eff19
andresbianchiispc <andresbianchi.ok@gmail.com>
2025-09-15 23:28:43 -0300

    creo carpetas y subo con consultasDDL.sql consultasDML.sql README.md

---
+++ DB-Evidencia-5/consultasDDL.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE smart_home_solutions;
+USE smart_home_solutions;
+
+CREATE TABLE usuarios(
+	id_usuario INT PRIMARY KEY NOT NULL,
+	nombre VARCHAR(50),
+	email VARCHAR (50)
+);
---
+++ DB-Evidencia-5/consultasDML.sql
@@ -0,0 +1,4 @@
+INSERT INTO usuarios (id_usuario, nombre, email) VALUES
+(1, 'Ana', 'ana.garcia@email.com'),
+(2, 'Luis', 'luis.perez@email.com');
+
