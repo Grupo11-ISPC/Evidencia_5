6fc596f18a0fc817b1b8d8897aae70aa3229e56f
Gabriel Farias <54718244+Gabbal@users.noreply.github.com>
2025-09-16 00:01:57 -0300

    Insertar dispositivos

--- DB-Evidencia-5/consultasDML.sql
+++ DB-Evidencia-5/consultasDML.sql
@@ -2,3 +2,8 @@ INSERT INTO usuarios (id_usuario, nombre, email) VALUES
 (1, 'Ana', 'ana.garcia@email.com'),
 (2, 'Luis', 'luis.perez@email.com');
 
+INSERT INTO dispositivos (nombre_dispositivo, tipo_dispositivo, estado_dispositivo, id_usuario) VALUES
+('luz Sala', 'Iluminación', 1, 1),
+('Termostato', 'Climatización', 0, 1),
+('Enchufe Cocina', 'Energía', 1, 2),
+('Sensor Puerta', 'Seguridad', 0, 2);
