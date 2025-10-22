905f26e008bd6adf1efa28287350ef04b94a0ec0
andresbianchiispc <andresbianchi.ok@gmail.com>
2025-10-22 00:29:27 -0300

    corrijo para que cumpla con el principio de responsabilidad unica

--- POO-SmartHome/dominio/usuario.py
+++ POO-SmartHome/dominio/usuario.py
@@ -1,62 +1,37 @@
 class Usuario:
-    def __init__(self, nombre, email, password, rol,id=None):
-        if not Usuario.validar_usuario(nombre, email, password, rol):
-            raise ValueError("Datos de usuario inválidos.")
-        self.__id = id
-        self.__nombre = nombre
-        self.__email = email
-        self.__password = password
-        self.__rol = rol
-        self.__dispositivos = []
-        self.__automatizaciones = []
-        
+    def __init__(self, nombre, email, password, rol, id=None):
+        self._id = id
+        self._nombre = nombre
+        self._email = email
+        self._password = password
+        self._rol = rol
 
-    def agregar_dispositivo(self, dispositivo):
-        self.__dispositivos.append(dispositivo)
-        return f"Dispositivo {dispositivo.get_nombre_dispositivo()} agregado."
-    
-    def eliminar_dispositivo(self, dispositivo):
-        if dispositivo in self.__dispositivos:
-            self.__dispositivos.remove(dispositivo)
-            return f"Dispositivo {dispositivo.get_nombre_dispositivo()} eliminado."
-        return "Dispositivo no encontrado."     
-    
-    def agregar_automatizacion(self, automatizacion):
-        self.__automatizaciones.append(automatizacion)
-        return f"Automatización {automatizacion.get_descripcion()} agregada."
-    
-    def listar_dispositivos(self):
-        return [dispositivo.get_nombre_dispositivo() for dispositivo in self.__dispositivos]
-    
-    def listar_automatizaciones(self):
-        return [automatizacion.get_descripcion() for automatizacion in self.__automatizaciones]
-    
+    def get_id(self):
+        return self._id
+        
     def get_nombre(self):
-        return self.__nombre
+        return self._nombre
     
     def get_email(self):
-        return self.__email
+        return self._email
     
     def set_nombre(self, nombre):
-        self.__nombre = nombre
+        self._nombre = nombre
 
     def set_email(self, email):
-        self.__email = email
+        self._email = email
 
     def set_password(self, password):
-        self.__password = password
+        self._password = password
 
-    def ejecutar_automatizaciones(self):
-        resultados = []
-        for automatizacion in self.__automatizaciones:
-            resultados.extend(automatizacion.ejecutar(self.__dispositivos))
-        return resultados
+    def get_password(self):
+        return self._password
     
-    def ejecutar_accion_en_dispositivo(self, dispositivo_nombre, accion):
-        for dispositivo in self.__dispositivos:
-            if dispositivo.get_nombre_dispositivo() == dispositivo_nombre:
-                return dispositivo.ejecutar_accion(accion)
-        return "Dispositivo no encontrado." 
+    def get_rol(self):
+        return self._rol
+    
+    def set_rol(self, rol):
+        self._rol = rol
 
     @staticmethod #para que no necesite self, osea, para que se valide sin instanciar la clase(crear el objeto)
     def validar_usuario(nombre, email, password, rol):
@@ -69,9 +44,3 @@ class Usuario:
         if rol not in ['admin', 'user']:
             return False
         return True
-    
-    def get_rol(self):
-        return self.__rol
-    
-    def set_rol(self, rol):
-        self.__rol = rol
