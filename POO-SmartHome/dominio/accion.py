f7d50f04a11b05310c66e0affce3de8cf5b02094
andresbianchiispc <andresbianchi.ok@gmail.com>
2025-10-26 18:08:58 -0300

    Cambio nomeclatura para que los atributos y métodos queden protegidos

--- POO-SmartHome/dominio/accion.py
+++ POO-SmartHome/dominio/accion.py
@@ -1,22 +1,22 @@
 class Accion:
     def __init__(self, tipo_accion, valor_configurado):
-        self.__tipo_accion = tipo_accion
-        self.__valor_configurado = valor_configurado
+        self._tipo_accion = tipo_accion
+        self._valor_configurado = valor_configurado
 
     def __str__(self):
-        return f"Accion: {self.__tipo_accion}, Valor Configurado: {self.__valor_configurado}"
+        return f"Accion: {self._tipo_accion}, Valor Configurado: {self._valor_configurado}"
     
     def get_tipo_accion(self):
-        return self.__tipo_accion
+        return self._tipo_accion
     
     def get_valor_configurado(self):
-        return self.__valor_configurado
+        return self._valor_configurado
     
     def set_tipo_accion(self, tipo_accion):
-        self.__tipo_accion = tipo_accion
+        self._tipo_accion = tipo_accion
         
     def set_valor_configurado(self, valor_configurado):
-        self.__valor_configurado = valor_configurado
+        self._valor_configurado = valor_configurado
 
     def realizar_accion(self):
-        return f"Realizando acción: {self.__tipo_accion} con valor: {self.__valor_configurado}"
+        return f"Realizando acción: {self._tipo_accion} con valor: {self._valor_configurado}"
--- POO-SmartHome/dominio/automatizacion.py
+++ POO-SmartHome/dominio/automatizacion.py
@@ -1,29 +1,29 @@
 
 class Automatizacion:
     def __init__(self, descripcion, condicion=None):
-        self.__descripcion = descripcion
-        self.__condicion = condicion
-        self.__acciones = []
+        self._descripcion = descripcion
+        self._condicion = condicion
+        self._acciones = []
 
     def agregar_accion(self, accion):
-        self.__acciones.append(accion)
+        self._acciones.append(accion)
         return f"Acción {accion} agregada a la automatización."
 
     def ejecutar(self, dispositivos):
         resultados = []
-        for accion in self.__acciones:
+        for accion in self._acciones:
             for dispositivo in dispositivos:
                 resultados.append(dispositivo.ejecutar_accion(accion))
         return resultados
 
     def set_descripcion(self, descripcion):
-        self.__descripcion = descripcion
+        self._descripcion = descripcion
 
     def get_descripcion(self):
-        return self.__descripcion
+        return self._descripcion
 
     def set_condicion(self, condicion):
-        self.__condicion = condicion
+        self._condicion = condicion
 
     def get_condicion(self):
-        return self.__condicion
+        return self._condicion
